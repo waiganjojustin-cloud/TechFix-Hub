1dd4ccd1f7faf402fa0f9cdccacc7f0268855581
Waiganjojustine <waiganjojustin@gmail.com>
2026-08-04 12:14:08 +0300

    Add a call function for updateClock

--- home.js
+++ home.js
@@ -63,3 +63,16 @@ function updateClock() {
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
+
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+
+    clock.innerHTML = `
+        ${dayName}<br>
+        ${day} ${month} ${year}<br>
+        ${hours}:${minutes}:${seconds}
+    `;
+
+}
+updateClock();
