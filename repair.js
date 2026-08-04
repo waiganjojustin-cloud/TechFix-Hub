a60a8839e53359a851aae6f9a6672b488e661e9c
Waiganjojustine <waiganjojustin@gmail.com>
2026-08-04 12:37:25 +0300

    Add variables to get form elements

---
+++ repair.js
@@ -0,0 +1,3 @@
+const repairForm = document.getElementById("repairForm");
+const paymentMethod = document.getElementById("paymentMethod");
+const mpesaSection = document.getElementById("mpesaSection");
