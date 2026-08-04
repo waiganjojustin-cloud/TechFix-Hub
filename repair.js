2368b9609304db1c1d6bb1b7e0d2f9eb6b26217e
Waiganjojustine <waiganjojustin@gmail.com>
2026-08-04 12:38:33 +0300

    Add function for to listen for change

--- repair.js
+++ repair.js
@@ -1,3 +1,23 @@
 const repairForm = document.getElementById("repairForm");
 const paymentMethod = document.getElementById("paymentMethod");
 const mpesaSection = document.getElementById("mpesaSection");
+
+if (paymentMethod) {
+
+    paymentMethod.addEventListener("change", function () {
+
+        if (paymentMethod.value === "M-Pesa") {
+
+            mpesaSection.style.display = "block";
+
+        } else {
+
+            mpesaSection.style.display = "none";
+
+            document.getElementById("paymentNumber").value = "";
+
+        }
+
+    });
+
+}
