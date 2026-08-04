08fbd9ee9c15014c259d8fa53b2e1dbfdb1e5c47
Waiganjojustine <waiganjojustin@gmail.com>
2026-08-04 12:18:16 +0300

    WIP: card animation function

--- home.js
+++ home.js
@@ -77,3 +77,20 @@ function updateClock() {
 }
 updateClock();
 setInterval(updateClock, 1000);
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(function (card, index) {
+
+    card.style.opacity = "0";
+    card.style.transform = "translateY(30px)";
+
+    setTimeout(function () {
+
+        card.style.transition = "0.6s ease";
+        card.style.opacity = "1";
+        card.style.transform = "translateY(0)";
+
+    }, index * 200);
+
+});
